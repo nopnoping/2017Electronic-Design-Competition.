7e857185e290ccd1e555c7445d1001ddbc383696
TroyeCriss <10457192@qq.com>
2019-01-26 20:06:38 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -8,6 +8,18 @@
 import sensor,image,time,struct,math
 from pyb import UART,LED,Timer
 threshold_black=[(0,100)]
+#找到小车返回飞控的数据帧
+def return_fc_find_car():
+    SUM=(0xBB+0x06+0x60+0xBB+0x01+0xaa)&0xff
+    temp=struct.pack("<BBBBBBB",
+                        0xBB,
+                        0x06,
+                        0x60,
+                        0xBB,
+                        0x01,
+                        0xaa,
+                        SUM)
+    uart.write(temp)
 #锁定圆
 def lock_place_circle(circles):
     global front_circle,current_circle
@@ -25,7 +37,7 @@ def lock_place_circle(circles):
 def lock_car(circles):
     global front_circle,current_circle,g_find_car,g_find_place_circle,g_find_flag
     if g_find_car==0:
-        #print("寻找小车")
+        #寻找小车
         i=0
         for c in circles:
             i+=1
@@ -33,6 +45,7 @@ def lock_car(circles):
             current_circle.x=c.x()
             current_circle.y=c.y()
             current_circle.r=c.r()
+        #图片中有两个圆时，离之前那个圆最远的为小车圆
         if i == 2:
             offset=0
             for c in circles:
@@ -42,16 +55,19 @@ def lock_car(circles):
                     current_circle.x=c.x()
                     current_circle.y=c.y()
                     current_circle.r=c.r()
-        elif abs(front_circle.y-current_circle.y)>20 and g_find_flag:
-            g_find_car=1
-        if(g_find_place_circle):
+            return_fc_find_car()
+        #图片中只有一个圆时，判断y坐标的偏移
+        elif i==1:
+            if abs(current_circle.y-front_circle.y)>15 and g_find_flag:
+                g_find_car=1
+                return_fc_find_car()
+        if g_find_place_circle:
             g_find_flag=1
-
         front_circle.x=current_circle.x
         front_circle.y=current_circle.y
         front_circle.r=current_circle.r
+        #锁定小车
     elif g_find_car==1:
-        #print("锁定小车")
         current_circle.x=500
         current_circle.y=500
         for c in circles:
@@ -89,7 +105,7 @@ def pack_data():
         x_high=(current_circle.x&0xff00)>>8
         x_low=current_circle.x&0xff
         y_high=0
-        y_low=25
+        y_low=10
         SUM=(0xBB+0x60+0x06+0xBF+0x04+x_high+x_low+y_high+y_low)&0xff
         temp=struct.pack("<BBBBBBBBBB",
                             0xBB,       #帧头
@@ -139,19 +155,19 @@ def receive_data():
     global g_mode,g_find_car,g_find_place_circle,g_find_flag
     if uart.any():
         a=uart.read()
-        '''检验数据帧头'''
         if a[0]==0xBB and a[1]==0x60 and a[2]==0x06:
             mode=a[5]
-            SUM=a[0]+a[1]+a[2]+a[3]+a[4]+a[5]
+            SUM=(a[0]+a[1]+a[2]+a[3]+a[4]+a[5])&0xff
             if SUM==a[6]:
-                #confirm_data()
+                confirm_data()
                 if mode==1:
                     g_mode=0
-                elif mode==2:
-                    g_mode=1
                     g_find_car=0
                     g_find_place_circle=0
                     g_find_flag=0
+                elif mode==2:
+                    g_mode=1
+
 
 #时钟回调
 def over_time(timer):
@@ -170,6 +186,7 @@ flag=True
 g_find_car=0    #寻找小车，0时未找到，1时找到
 g_find_place_circle=0
 g_find_flag=0
+g_no_circle=0
 #摄像头传感器设置
 sensor.reset()
 sensor.set_pixformat(sensor.GRAYSCALE)
@@ -186,36 +203,30 @@ timer.callback(over_time)
 led2=LED(2)
 led2.on()
 led3=LED(3)
-led3.off()
 clock=time.clock()
 
 while True:
-    clock.tick()
     #修正镜头畸变
     img=sensor.snapshot().lens_corr(1.8)
-    #img.binary(threshold_black)
     c=img.find_circles(threshold=4000,x_margin=10,y_margin=10,r_margin=10)
     find_circle=False
     if c:
         find_circle=True
         if g_mode==0:
-            led2.on()
-            led3.off()
-            #print("锁定场地圆")
+            #锁定场地圆
             lock_place_circle(c)
-            #pack_data()
-            #img.draw_circle(current_circle.x,current_circle.y,current_circle.r,color=(255,0,0))
         elif g_mode==1:
-            led2.off()
-            led3.on()
+            #锁定小车
             lock_car(c)
-            #img.draw_circle(current_circle.x,current_circle.y,current_circle.r,color=(255,0,0))
     if flag:
         flag=False
         receive_data()
-        print(g_mode)
+        if g_mode==0:
+            led2.on()
+            led3.off()
+        elif g_mode==1:
+            led2.off()
+            led3.on()
         if find_circle:
             find_circle=False
             pack_data()
-    #print(flag)
-    #print(clock.fps())
